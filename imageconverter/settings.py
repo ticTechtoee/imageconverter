c6b51a46b4e742303e3c872cfa77406e4bdc6b1b
Farhan Basheer <farhanfb6@outlook.com>
2022-10-28 22:03:49 +0500

    Updated Settings.py file

--- imageconverter/settings.py
+++ imageconverter/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-6s*3^u7y^39f97x266wlp0qb3f*hwp=py(vk2b=q_b#9d1m43$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
